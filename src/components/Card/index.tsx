7dda7fa9c21273be84dd319c09a62fe14bc9b336
Toto Rubianto <toto.rubianto.17@gmail.com>
2020-09-19 16:30:53 +0700

    add card

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,48 @@
+import React, { useRef } from 'react';
+import clsx from 'clsx';
+import styles from './styles/card.style.scss';
+import { Ripple } from '../Ripple';
+
+interface ICard {
+  className?: string,
+  variant?: string,
+  active?: string,
+  children?: any,
+  onClick?: any,
+  value?: string
+}
+
+const Card = ({
+  className,
+  variant,
+  active,
+  children,
+  onClick,
+  value
+}: ICard) => {
+  const rippleRef: any = useRef()
+
+  const handleClick = (e: any) => {
+    if (rippleRef?.current) rippleRef.current.addRipple(e)
+    if (typeof onClick === 'function') onClick(value)
+  }
+  return (
+    <div
+      className={clsx(
+        styles.card,
+        variant === 'outlined' && styles.outline,
+        active && styles.active,
+        typeof onClick === 'function' && styles.selectable,
+        className
+      )}
+      onClick={handleClick}
+    >
+      {children}
+      {
+        typeof onClick === 'function' && <Ripple ref={rippleRef} />
+      }
+    </div>
+  )
+}
+
+export default Card;
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,2 +1,4 @@
 export * from './Button'
 export * from './Loading'
+export * as Icon from './Icon'
+export * from './Card'
